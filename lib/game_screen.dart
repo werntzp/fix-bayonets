fb5cbfa1daa9c0e8cac29cb77161123cd8601722
werntzp <werntzp@hotmail.com>
2020-12-27 21:03:33 -0500

    Tweaked colors

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -8,7 +8,8 @@ class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff6b8e23),
+      backgroundColor: const Color(0xffd3d3d3),
+      //backgroundColor: const Color(0xff6b8e23),
       //backgroundColor: const Color(0xff556600),
       body: Container(
         margin: const EdgeInsets.only(left: 10.0, right: 10.0),
@@ -31,165 +32,67 @@ class GameScreen extends StatelessWidget {
               padding: const EdgeInsets.all(10.0),
               child: Container(
                 decoration: new BoxDecoration(
+                  color: const Color(0xff6b8e23),
                   border: Border.all(
                     width: 3,
                   ),
                 ),
-                height: 450.0,
+                height: 370.0,
                 child: GridView.count(
                   crossAxisCount: 8,
-                  children: <Widget>[
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
+                  children: List.generate(64, (index) {
+                    return Container(
+                      decoration: new BoxDecoration(
+                        border: Border.all(
+                          width: 1,
                         ),
-                        child: Text("Cell 1")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 2")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 4")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 5")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 6")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 7")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 8")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 9")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 10")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 11")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                    Container(
-                        decoration: new BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                          ),
-                        ),
-                        child: Text("Cell 3")),
-                  ],
+                      ),
+                      child: Center(child: Text('$index')),
+                    );
+                  }),
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Align(
+                  alignment: Alignment.topCenter, child: Text("Your hand:")),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  decoration: new BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                    ),
+                  ),
+                  height: 85.0,
+                  width: 60.0,
+                  child: Image.asset('images/bayonet.png'),
+                ),
+                Container(
+                  decoration: new BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                    ),
+                  ),
+                  height: 85.0,
+                  width: 60.0,
+                  child: Image.asset('images/pistol.png'),
+                ),
+                Container(
+                  decoration: new BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                    ),
+                  ),
+                  height: 85.0,
+                  width: 60.0,
+                  child: Image.asset('images/wire.png'),
+                ),
+              ],
+            )
           ],
         ),
       ),
